4c8c82a860933c25ccdd1758f24d932c807e7b71
nvazquez <nicolas.m.vazquez@gmail.com>
2016-02-01 18:29:41 -0200

    Copy multiple files to server script

---
+++ copyMultipleFilesToServer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 2016-02-01
+# Nicolas Vazquez
+# --------------------------------------------------------------------------------------------------------------------------
+# Script to copy multiple files from source to dest host, using scp.
+# PRE: Configure id_rsa.pub key file and add it to dest authorized_keys file, to not ask for password in every scp command
+# --------------------------------------------------------------------------------------------------------------------------
+# USAGE: ./copyMultipleFilesToServer.sh file1 file2 file3 ... fileN
+
+intro(){
+	echo
+	echo "************************Copy Files to Server************************"
+	echo "Number of files to copy: $1"
+	echo "********************************************************************"
+	echo
+}
+
+# Variables
+SRC_HOST_BASE_ROUTE="/home/XXX"
+DEST_HOST_USER="YYY"
+DEST_HOST_IP="ZZ.ZZ.ZZ.ZZ"
+DEST_HOST_BASE_ROUTE="/home/WW"
+
+# Pass params number to intro function
+intro "$#"
+
+# Store arguments in array
+arguments=("$@") 
+N=${#arguments[@]} 
+
+# Copy files through scp (having id_rsa.pub key file on server's authorized_keys)
+oneLineFiles=""
+for (( i=0;i<$N;i++)); do 
+    arg=${arguments[${i}]}
+	scp $SRC_HOST_BASE_ROUTE/$arg $DEST_HOST_USER@$DEST_HOST_IP:$DEST_HOST_BASE_ROUTE/$arg
+done
+
+echo
+echo "Transfer completed."
+echo "********************************************************************"
