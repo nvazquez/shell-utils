c9c21c5ee0307bb539025104324e70c04d5ecd90
nvazquez <nicolas.m.vazquez@gmail.com>
2016-02-04 16:08:06 -0200

    Adding params

--- copyMultipleFilesToServer.sh
+++ copyMultipleFilesToServer.sh
@@ -6,33 +6,63 @@
 # Script to copy multiple files from source to dest host, using scp.
 # PRE: Configure id_rsa.pub key file and add it to dest authorized_keys file, to not ask for password in every scp command
 # --------------------------------------------------------------------------------------------------------------------------
-# USAGE: ./copyMultipleFilesToServer.sh file1 file2 file3 ... fileN
 
 intro(){
 	echo
+	echo
 	echo "************************Copy Files to Server************************"
 	echo "Number of files to copy: $1"
+	echo
+	echo "Details:"
+	echo
+	echo "local_base_folder = "$SRC_HOST_BASE_ROUTE
+	echo
+	echo "dst_host_ip_address = "$DEST_HOST_IP
+	echo "dst_host_folder = "$DEST_HOST_BASE_ROUTE
+	echo "dst_host_user = "$DEST_HOST_USER
 	echo "********************************************************************"
+	echo
 	echo
 }
 
 display_help(){
-	echo "Usage: copyMultipleFilesToServer [-h <host_ip>] file1 ... [fileN]"
+	echo "usage: copyMultipleFilesToServer [-b <local_base_folder>]"
+	echo "                                 [-H <dst_host_ip_address>] [-d <dst_host_folder>] [-u <dst_host_user>]"
+	echo "                                 file1 [... fileN]"
+	echo
 }
 
-while getopts ':hs:' option; do
+# Variables
+SRC_HOST_BASE_ROUTE="/home/XXX"			# -b route
+DEST_HOST_IP="ZZ.ZZ.ZZ.ZZ"				# -H ip
+DEST_HOST_BASE_ROUTE="/home/WW"			# -d route
+DEST_HOST_USER="YYY"					# -u user
+
+# getopts tutorial
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+while getopts 'hH:u:b:d:' option; do
 	case "$option" in
 	h) 	display_help
+		exit
+		;;
+	b)	SRC_HOST_BASE_ROUTE=$OPTARG
+		;;
+	u)	DEST_HOST_USER=$OPTARG
+		;;
+	H)	DEST_HOST_IP=$OPTARG
+		;;
+	d)	DEST_HOST_BASE_ROUTE=$OPTARG
+		;;
+	\?)	echo "Invalid option"
 		exit
 		;;
 	esac
 done
 
-# Variables
-SRC_HOST_BASE_ROUTE="/home/XXX"
-DEST_HOST_USER="YYY"
-DEST_HOST_IP="ZZ.ZZ.ZZ.ZZ"
-DEST_HOST_BASE_ROUTE="/home/WW"
+echo $SRC_HOST_BASE_ROUTE
+echo $DEST_HOST_IP
+echo $DEST_HOST_USER
+echo $DEST_HOST_BASE_ROUTE
 
 # Pass params number to intro function
 intro "$#"
